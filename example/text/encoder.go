25dcd493e6f6c557cfddb6513f74fed985502787
Givi Odikadze <givi.odikadze@gmail.com>
2019-06-08 13:12:08 -0400

    fixed bug with slice pointer

--- example/text/encoder.go
+++ example/text/encoder.go
@@ -25,7 +25,7 @@ func NewEncoder(letters *Letters) *Encoder {
 func (e *Encoder) encodeLanguage(text *string) *hyperdimensional.VecBinomial {
 	e.profile = nil
 	e.counter = 0
-	gramChannel := make(chan *[]uint8)
+	gramChannel := make(chan []uint8)
 	wg := new(sync.WaitGroup)
 
 	for i := 0; i < 10; i++ {
@@ -33,18 +33,18 @@ func (e *Encoder) encodeLanguage(text *string) *hyperdimensional.VecBinomial {
         go e.encodeGram(gramChannel, wg)
 	}
 	
-	indices := make([]uint8, GramFactor)
 	e.totalCount = len(*text) - GramFactor
     for textIndex := range *text {
     	if textIndex > len(*text) - GramFactor {
     		break
 		}
 
-        for index := range indices {
-            indices[index] = (*text)[textIndex + index]
+		asciiLetters := make([]uint8, GramFactor)
+        for index := range asciiLetters {
+            asciiLetters[index] = (*text)[textIndex + index]
 		}
 
-        gramChannel <- &indices
+        gramChannel <- asciiLetters
 	}
 
 	close(gramChannel)
@@ -54,20 +54,20 @@ func (e *Encoder) encodeLanguage(text *string) *hyperdimensional.VecBinomial {
 	return e.profile
 }
 
-func (e *Encoder) encodeGram(textIndicesChannel chan *[]uint8, wg *sync.WaitGroup) {
+func (e *Encoder) encodeGram(AsciiLettersChannel chan []uint8, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for textIndices := range textIndicesChannel {
+	for asciiLetters := range AsciiLettersChannel {
 		var gram *hyperdimensional.VecBinomial
-		for i, textIndex := range *textIndices {
+		for i, textIndex := range asciiLetters {
 			if i == 0 {
-				gram = hyperdimensional.Rotate(e.letters[textIndex], len(*textIndices) - 1)
+				gram = hyperdimensional.Rotate(e.letters[textIndex], len(asciiLetters) - 1)
 				continue
 			}
 
 			next := e.letters[textIndex]
-			if len(*textIndices) - i - 1 != 0 {
-				next = hyperdimensional.Rotate(next, len(*textIndices) - i - 1)
+			if len(asciiLetters) - i - 1 != 0 {
+				next = hyperdimensional.Rotate(next, len(asciiLetters) - i - 1)
 			}
 
 			gram = hyperdimensional.Multiply(gram, next)
